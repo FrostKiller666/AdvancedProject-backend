504d21ca8ec773de75422a3ed911f5ce8eb2adc4
FrostKiller <bialywilk50@gmail.com>
2022-06-18 17:13:45 +0200

    Added hashed password when inert to DP.

--- routes/user.router.ts
+++ routes/user.router.ts
@@ -1,5 +1,7 @@
 import {Router} from "express";
 import {UserRecord} from "../records/user.record";
+import {hash} from 'bcrypt'
+import {ValidationError} from "../utils/errrors";
 
 export const userRouter = Router()
 
@@ -8,8 +10,14 @@ export const userRouter = Router()
     })
     .post('/register', async (req, res) => {
         const user = new UserRecord(req.body);
-        console.log(user)
-        await user.insert();
+        hash(user.password, 10, async (err, hash) => {
+            if (err) {
+                throw new ValidationError(`${err}`);
+            } else {
+                user.password = hash;
+                await user.insert();
 
-        res.json(user);
+                res.json(user);
+            }
+        });
     });
